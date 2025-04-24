2064ff6368661c1fc66dbcfdf0e906f0b42cfbee
jailson-p <jailson.p@aluno.ifsc.edu.br>
2025-04-24 10:40:23 -0300

    Ajustes para o pactote princeton e o .gitignore

--- app/src/main/java/ifsc/poo/App.java
+++ app/src/main/java/ifsc/poo/App.java
@@ -3,12 +3,15 @@
  */
 package ifsc.poo;
 
+import edu.princeton.cs.algs4.Draw;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Draw asdf;
     }
 }
